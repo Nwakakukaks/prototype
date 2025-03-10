c7b8669d2f8c96744670558c932fa656b7a4ec0a
nwakakukaks <winnernwakaku123@gmail.com>
2025-03-10 12:40:09 +0100

    push

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,36 +1,29 @@
 import CharacterSelect from "@/components/CharacterSelect";
-import CreateCharacterButton from "@/components/CreateCharacterButton";
 import { CharacterSelectProvider } from "@/contexts/CharacterSelectContext";
 import { Providers } from "@/providers/providers";
 import {
   ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  UserButton,
 } from "@clerk/nextjs";
 import type { Metadata } from "next";
-import Image from "next/image";
-import { inter } from "./fonts";
+import { inter, pixelify_sans } from "./fonts";
 import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
-import { NavBar } from "@/components/navbar";
 
 export const metadata: Metadata = {
-  title: "RequestForStartup - Electronuem DEFAI Project",
+  title: "Prototype.ai - Make something people want",
   description:
-    "RequestForStartup is an Electronuem DEFAI project exploring recursive agent inference and natural language interfaces. Experience a unified platform where AI agents interact, learn, and evolve through natural conversations while leveraging blockchain technology.",
+    "Prototype.ai is an AI project exploring recursive agent inference and natural language interfaces. Experience a unified platform where AI agents interact, learn, and evolve through natural conversations while leveraging blockchain technology.",
   keywords:
-    "Electronuem DEFAI, Hackathon, AI Agents, Recursive Inference, Natural Language Interface, Blockchain, Web3",
+    "Electronuem, Hackathon, AI Agents, Recursive Inference, Natural Language Interface, Blockchain, Web3",
   openGraph: {
-    title: "RequestForStartup - Recursive Agent Platform",
+    title: "Prototype.ai - Make something people want",
     description:
-      "Explore the future of AI agent interactions through natural language on a unified blockchain platform. Built at Electronuem DEFAI.",
+      "Explore the future of AI agent interactions through natural language on a unified blockchain platform. Built at Electronuem hack.",
     type: "website",
   },
   twitter: {
     card: "summary_large_image",
-    title: "RequestForStartup - Electronuem DEFAI",
+    title: "Prototype.ai - Make something people want",
     description:
       "Revolutionary platform for recursive AI agent interactions through natural language interfaces.",
   },
@@ -49,11 +42,13 @@ export default function RootLayout({
             <Providers>
               <CharacterSelectProvider>
                 <CharacterSelect />
-                <div className="text-sm p-1 text-center bg-orange-50 text-gray-900">
-                  We&apos;re launching soon on product hunt! Please support by leaving
-                  us an upvote 🙏
+                <div
+                  className={`${pixelify_sans.className} text-sm p-1 text-center bg-orange-50 text-gray-900`}
+                >
+                  We&apos;re launching soon on product hunt! Please support by
+                  leaving us an upvote 🙏
                 </div>
-               
+
                 {children}
               </CharacterSelectProvider>
             </Providers>
